f52ff8033d63b572b9aaf7fcc743c9c1ca4bd60a
mathiasbald <mathiasbaldissra@hotmail.com>
2017-10-28 11:16:48 -0200

    Alteração para a interface e upload do astah

--- src/catalogo/gerenciadores/GerenciadorDeMidias.java
+++ src/catalogo/gerenciadores/GerenciadorDeMidias.java
@@ -3,39 +3,38 @@ package catalogo.gerenciadores;
 import catalogo.midias.Midia;
 import java.util.List;
 
-public abstract class GerenciadorDeMidias {
+public abstract class GerenciadorDeMidias implements IGerenciador {
 
-	private List midias;
+    private List midias;
 
-	
-	public boolean cadastrar(Midia midia) {
-		return false;
-	}
+    @Override
+    public boolean cadastrar(Midia midia) {
+        return false;
+    }
 
-	public boolean remover(String path) {
-		return false;
-	}
+    @Override
+    public boolean remover(String path) {
+        return false;
+    }
 
+    @Override
+    public Midia consultar(String titulo) {
+        return null;
+    }
 
-	public Midia consultar(String titulo) {
-		return null;
-	}
+    @Override
+    public String exibir() {
+        return null;
+    }
 
-	public String exibir() {
-		return null;
-	}
+    @Override
+    public boolean editar(String titulo) {
+        return false;
+    }
 
-	public abstract void  ordenar();
-
-	public boolean editar(String titulo) {
-		return false;
-	}
-
-
-	public boolean salvarArquivo() {
-		return false;
-	}
-
-	public abstract boolean  carregarArquivo();
+    @Override
+    public boolean salvarArquivo() {
+        return false;
+    }
 
 }
---
+++ src/catalogo/gerenciadores/IGerenciador.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catalogo.gerenciadores;
+
+import catalogo.midias.Midia;
+
+/**
+ *
+ * @author mathias
+ */
+public interface IGerenciador {
+
+    public boolean cadastrar(Midia midia);
+
+    public boolean remover(String path) ;
+
+    public Midia consultar(String titulo) ;
+
+    public String exibir() ;
+
+    public abstract void ordenar();
+
+    public boolean editar(String titulo);
+
+    public boolean salvarArquivo();
+
+    public abstract boolean carregarArquivo();
+
+}
