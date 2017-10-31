fc708b0f44e400fe0dbf6b542175d1374c6d6191
Unknown <walkertoledo@gmail.com>
2017-10-31 20:52:10 -0200

    Getter e setter
    AudioLivro e no PartituraMusical

--- src/catalogo/midias/AudioLivro.java
+++ src/catalogo/midias/AudioLivro.java
@@ -2,21 +2,88 @@ package catalogo.midias;
 
 
 public class AudioLivro extends Midia {
-
 	private int genero;
-
 	private String idioma;
-
 	private String autores;
-
 	private String local;
-
 	private String editora;
-
 	private int duracao;
-
 	private int ano;
-
 	private Midia midia;
+
+    public AudioLivro(int genero, String idioma, String autores, String local, String editora, int duracao, int ano, Midia midia) {
+        this.genero = genero;
+        this.idioma = idioma;
+        this.autores = autores;
+        this.local = local;
+        this.editora = editora;
+        this.duracao = duracao;
+        this.ano = ano;
+        this.midia = midia;
+    }
+
+    public int getGenero() {
+        return genero;
+    }
+
+    public void setGenero(int genero) {
+        this.genero = genero;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Midia getMidia() {
+        return midia;
+    }
+
+    public void setMidia(Midia midia) {
+        this.midia = midia;
+    }
 
 }
--- src/catalogo/midias/PartituraMusical.java
+++ src/catalogo/midias/PartituraMusical.java
@@ -1,13 +1,48 @@
 package catalogo.midias;
 
 public class PartituraMusical extends Midia {
-
 	private String genero;
-
 	private String autores;
-
 	private String instrumentos;
-
 	private int ano;
+
+    public PartituraMusical(String genero, String autores, String instrumentos, int ano) {
+        this.genero = genero;
+        this.autores = autores;
+        this.instrumentos = instrumentos;
+        this.ano = ano;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public String getInstrumentos() {
+        return instrumentos;
+    }
+
+    public void setInstrumentos(String instrumentos) {
+        this.instrumentos = instrumentos;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
 
 }
