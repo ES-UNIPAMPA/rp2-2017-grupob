a9fbf4e8a89c87adbbd1eb66449ac32f3fdc5553
Ualquer <walkertoledo@gmail.com>
2017-11-07 21:59:54 -0200

    Metodo editar AudioLivro
    Começo da implementação do metodo editar

--- src/catalogo/guis/GUIAudioLivros.java
+++ src/catalogo/guis/GUIAudioLivros.java
@@ -1,5 +1,6 @@
 package catalogo.guis;
 
+import catalogo.EntradasDoUsuarioComValidacao;
 import catalogo.gerenciadores.GAudioLivros;
 import catalogo.gerenciadores.IGerenciador;
 import catalogo.midias.AudioLivro;
@@ -9,35 +10,35 @@ import java.util.Scanner;
 public class GUIAudioLivros implements IMidiaUsuario {
 
     IGerenciador gerenciador = new GAudioLivros(new ArrayList<>());
-    Scanner entrada = new Scanner (System.in);
-    
-    String genero, idioma, autores, local, editora, titulo, descricao;
-    int ano;
-    float duracao;
+    EntradasDoUsuarioComValidacao entrada = new EntradasDoUsuarioComValidacao();
 
     @Override
     public void cadastro() {
 //String genero, String idioma, String autores, String local, String editora, int duracao, int ano, String titulo, String descricao, String path
+        String genero, idioma, autores, local, editora, titulo, descricao;
+        int ano;
+        float duracao;
+
         AudioLivro midia;
         System.out.println("Genero do AudioLivro: ");
-        genero = entrada.next();
+        genero = entrada.nextString();
         System.out.println("Idioma do AudioLivro: ");
-        idioma = entrada.next();
+        idioma = entrada.nextString();
         System.out.println("Autores do AudioLivro: ");
-        autores = entrada.next();
+        autores = entrada.nextString();
         System.out.println("Local do AudioLivro: ");
-        local = entrada.next();
+        local = entrada.nextString();
         System.out.println("Editora do AudioLivro: ");
-        editora = entrada.next();
+        editora = entrada.nextString();
         System.out.println("Duração do AudioLivro: ");
-        duracao = entrada.nextFloat();
+        duracao = (float) entrada.nextDouble(false);
         System.out.println("Ano do AudioLivro: ");
-        ano = entrada.nextInt();
+        ano = entrada.nextInt(false);
         System.out.println("Título do AudioLivro: ");
-        titulo = entrada.next();
+        titulo = entrada.nextString();
         System.out.println("Descrição do AudioLivro: ");
-        descricao = entrada.next();
-       
+        descricao = entrada.nextString();
+
         midia = new AudioLivro(genero, idioma, autores, local, editora, duracao, ano, titulo, descricao, local);
         gerenciador.cadastrar(midia);
     }
@@ -46,7 +47,7 @@ public class GUIAudioLivros implements IMidiaUsuario {
     public void exclusao() {
         String caminho;
         System.out.println("Digite o caminho do AudioLivro a ser excluido: ");
-        caminho = entrada.next();
+        caminho = entrada.nextString();
         gerenciador.remover(caminho);
 
     }
@@ -58,7 +59,67 @@ public class GUIAudioLivros implements IMidiaUsuario {
 
     @Override
     public void editar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String genero, idioma, autores, local, editora, titulo, descricao;
+        int ano;
+        double duracao;
+        //pede o audiolivro pro usuario
+        AudioLivro velho = gerenciador.consultar(titulo);
+        AudioLivro novo;
+
+        System.out.println("Novo genero do AudioLivro: ");
+        genero = entrada.nextString();
+        if (genero.equals("")) {
+            genero = velho.getGenero();
+        }
+        
+        System.out.println("Idioma do AudioLivro: ");
+        idioma = entrada.nextString();
+        if(idioma.equals("")){
+            idioma = velho.getIdioma();
+        }
+        
+        System.out.println("Autores do AudioLivro: ");
+        autores = entrada.nextString();
+        if(autores.equals("")){
+            autores = velho.getAutores();
+        }
+        
+        System.out.println("Local do AudioLivro: ");
+        local = entrada.nextString();
+        if(local.equals("")){
+            local = velho.getLocal();
+        }
+        
+        System.out.println("Editora do AudioLivro: ");
+        editora = entrada.nextString();
+        if(editora.equals("")){
+            editora = velho.getEditora();
+        }
+        
+        System.out.println("Duração do AudioLivro: ");
+        duracao = entrada.nextDouble(true);
+        if (duracao==Double.MIN_VALUE){
+            duracao = velho.getDuracao();
+        }
+        
+        System.out.println("Ano do AudioLivro: ");
+        ano = entrada.nextInt(true);
+        if(ano == Integer.MIN_VALUE){
+            ano = velho.getAno();
+        }
+        
+        System.out.println("Título do AudioLivro: ");
+        titulo = entrada.nextString();
+        if(titulo.equals("")){
+            titulo = velho.getTitulo();
+        }
+        
+        System.out.println("Descrição do AudioLivro: ");
+        descricao = entrada.nextString();
+        if(descricao.equals("")){
+            titulo = velho.getDescricao();
+        }
+
     }
 
     @Override
